88baf4de04cccdc2922a286b0474f589b5d2bfa3
Matteo Sabbatini <matteosabbatini02@gmail.com>
2023-09-29 16:54:55 +0200

    Visualizzazione notizie interne/esterne + visualizzazione commenti

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 #  resources :article do
 #    resources :comments
 #  end
+  root 'articles#index'
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/comments', to: 'comments#index', as: 'comments'
+
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+local_articles = [{:ext_id => nil,:title => "Apertura Sito", 
+            :img_url => 'https://pleated-jeans.com/wp-content/uploads/2020/07/wait-the-always-has-been-meme-is-still-funny-always-has-been-46-memes-6.jpg',
+            :body => "Benvenuti su Spacewalk.",
+            :local => true,
+            :author_id => nil,
+            :url => nil,
+            :news_site => nil,
+            :published_at => DateTime.new(2023,9,29,12,24,0)},
+        ]
+
+local_articles.each do |art|
+    Article.create!(art)
+end
+
+#mock user
+User.create!({:email => 'fake@mail.com', :encrypted_password => '#$taawtljaskt', :password => 'validpass',
+            :password_confirmation => 'validpass'})
+
+Comment.create!({:user => User.find(1), :text => 'Bello!', :published_at => DateTime.new(2023,9,29,12,25,0),
+                :article => Article.find_by(title: "Apertura Sito")})
