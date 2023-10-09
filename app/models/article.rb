0d827a0e516599ad5d4c4cda1876c7f270fe6c61
Matteo Sabbatini <matteosabbatini02@gmail.com>
2023-10-09 17:59:40 +0200

    User stories 1-4, 27-29

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,31 +15,38 @@ class Article < ActiveRecord::Base
     end
 
 
-    #inserisce AL MASSIMO 10 articoli piu recenti nel database, eseguita non piu di una volta ogni 3 ore 
+    #inserisce TUTTI gli articoli piu recenti nel database, eseguita non piu di una volta ogni 3 ore 
     def self.update_articles 
+        Rails.cache.clear
         if Rails.cache.read('up_to_date').nil?
-              uri = URI('https://api.spaceflightnewsapi.net/v4/articles/')
-              response_string = Net::HTTP.get(uri)
-              response = JSON.parse response_string
-              articles = response['results']
-              articles.each do |article|
-                  #puts(article['id'].to_s + ' | ' + article['title'])
-                  if Article.exists?(ext_id: article['id'])
-                      break
-                  else     
-                      art = Article.create({
-                        :ext_id => article['id'],
-                        :title => article['title'],
-                        :img_url => article['image_url'],
-                        :body => nil,
-                        :local => false,
-                        :author_id => nil,
-                        :url => article['url'],
-                        :news_site => article['news_site'],
-                        :published_at => article['published_at']
-                      })
-                  end
-              end
+            done = false
+            i = 0 #indice di sicurezza per non scaricare tutto il db remoto
+            uri = URI('https://api.spaceflightnewsapi.net/v4/articles/')
+            while !done and i < 50
+                response_string = Net::HTTP.get(uri)
+                response = JSON.parse response_string
+                articles = response['results']
+                articles.each do |article|
+                    if Article.exists?(ext_id: article['id'])
+                        done = true
+                        break
+                    else    
+                        art = Article.create({
+                            :ext_id => article['id'],
+                            :title => article['title'],
+                            :img_url => article['image_url'],
+                            :body => nil,
+                            :local => false,
+                            :author_id => nil,
+                            :url => article['url'],
+                            :news_site => article['news_site'],
+                            :published_at => article['published_at']
+                        })
+                    end   
+                end
+                uri = URI(response['next'])
+                i += 10 
+            end
             Rails.cache.write('up_to_date', 'true', expires_in: 3.hours)
             #flash[:notice] = 'updated'
         else
--- get_articles.rb
+++ get_articles.rb
@@ -4,32 +4,26 @@
 #req.ca_path = "/etc/ssl" 
 #response_string = req.get('/v4/articles/')
 
-#Script da eseguire periodicamente per inserire TUTTE(!) le notizie più recenti non presenti nel database
-
-done = false
+#Script da eseguire periodicamente per inserire AL MASSIMO 10 notizie più recenti non presenti nel database
 uri = URI('https://api.spaceflightnewsapi.net/v4/articles/')
-while !done
-    response_string = Net::HTTP.get(uri)
-    response = JSON.parse response_string
-    articles = response['results']
-    articles.each do |article|
-        if Article.exists?(ext_id: article['id'])
-            done = true
-            break
-        else    
-            puts(article['id'].to_s + ' | ' + article['title'] + ' | ' + + article['id'].to_s)
-=begin      art = Article.create({
-                :ext_id => article['id'],
-                :title => article['title'],
-                :img_url => article['image_url'],
-                :body => nil,
-                :local => false,
-                :author_id => nil,
-                :url => article['url'],
-                :news_site => article['news_site'],
-                :published_at => article['published_at']
-=end        })
-        end   
+response_string = Net::HTTP.get(uri)
+response = JSON.parse response_string
+articles = response['results']
+articles.each do |article|
+    if Article.exists?(ext_id: article['id'])
+        break
+    else
+        #puts(article['id'].to_s + ' | ' + article['title'] + ' | ' + + article['id'].to_s)     
+        art = Article.create({
+          :ext_id => article['id'],
+          :title => article['title'],
+          :img_url => article['image_url'],
+          :body => nil,
+          :local => false,
+          :author_id => nil,
+          :url => article['url'],
+          :news_site => article['news_site'],
+          :published_at => article['published_at']
+        })
     end
-    uri = URI(response['next']) 
 end
