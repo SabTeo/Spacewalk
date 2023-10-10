77de5b86fb067654deecc64a669e1e6f53007cfa
Matteo Sabbatini <matteosabbatini02@gmail.com>
2023-10-10 19:30:22 +0200

    Migrazione dimenticata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_095146) do
+ActiveRecord::Schema.define(version: 2023_10_10_172011) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
