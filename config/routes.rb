65b9d6adf1f442bebc87c012e2d613a69b051f67
manuel <manuel@Host.myguest.virtualbox.org>
2023-10-10 17:48:09 +0200

    Aggiunto errore in log in, edit profile e campo username in sign up

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/articles/:id/comments', to: 'comments#index', as: 'comments'
   post '/article/:id/comments', to: 'comments#create', as: 'new_comment'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
